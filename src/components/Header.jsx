5972b449e54fdf20a64c61312cd924e32836e2b4
noah1365 <borlagdatanjohnlloyd1@gmail.com>
2025-02-21 20:56:04 +0800

    ADD CREATE BUTTON WITH MODAL OPEN

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ title }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   return (
-    <header className="bg-white bg-opacity-50 backdrop-blur-md shadow-lg border border-black">
+    <header className="bg-white bg-opacity-50 backdrop-blur-md shadow-lg border border-black mb-10">
       <div className="max-w-7xl mx-auto py-4 px-4 sm:px-6 lg:px-8 flex items-center justify-between">
         {/* Left Section: Title */}
         <h1 className="text-2xl font-semibold text-black">{title}</h1>
--- src/private/admin/benefit/BenefitsManagement.jsx
+++ src/private/admin/benefit/BenefitsManagement.jsx
@@ -1,9 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Header from "../../../components/Header";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function BenefitsManagement() {
     const [allBenefits, setBenefits] = useState([]);
+    const [isOpenModal, setIsOpenModal] = useState(false);
+    const [formData, setFormData] = useState({
+        benefitName: "",
+        benefitDescription: "",
+        benefitType: "Others",
+        isNeedRequest: false,
+    });
 
     useEffect(() => {
         fetchBenefits();
@@ -18,10 +27,44 @@ function BenefitsManagement() {
         }
     };
 
+    const handleCreate = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post("http://localhost:7687/api/benefit/create-benefit", formData);
+            toast.success("Benefit created successfully!");
+            setFormData({
+                benefitName: "",
+                benefitDescription: "",
+                benefitType: "Others",
+                isNeedRequest: false,
+            });
+            fetchBenefits();
+            setIsOpenModal(false);
+        } catch (error) {
+            console.error("Error creating benefit:", error.response ? error.response.data : error.message);
+            toast.error("Error creating benefit: " + (error.response ? error.response.data.message : error.message));
+        }
+    };
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value,
+        });
+    };
+
     return (
         <div>
             <Header title="Benefits Management" />
-            
+            <ToastContainer />
+            <button
+                className="mb-4 px-4 py-2 btn btn-primary text-white rounded"
+                onClick={() => setIsOpenModal(true)}
+            >
+                Create Benefit
+            </button>
+
             <div>
                 <table className="min-w-full divide-y divide-gray-200">
                     <thead className="bg-gray-50">
@@ -35,14 +78,13 @@ function BenefitsManagement() {
                     </thead>
                     <tbody>
                         {allBenefits.length > 0 ? (
-                            allBenefits.map((benefit) => (       
+                            allBenefits.map((benefit) => (
                                 <tr key={benefit._id} className="hover:bg-gray-300 hover:text-white">
                                     <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider">{benefit.benefitName}</td>
                                     <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider">{benefit.benefitDescription}</td>
                                     <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider">{benefit.benefitType}</td>
                                     <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider">{benefit.isNeedRequest ? "Yes" : "No"}</td>
-                                    <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider">
-                                    </td>
+                                    <td className="px-6 py-4 text-left text-xs font-semibold text-neutral uppercase tracking-wider"></td>
                                 </tr>
                             ))
                         ) : (
@@ -51,8 +93,71 @@ function BenefitsManagement() {
                             </tr>
                         )}
                     </tbody>
-                </table>   
+                </table>
             </div>
+
+            {isOpenModal && (
+                <div className="fixed inset-0 flex items-center justify-center bg-gray-600 bg-opacity-50">
+                    <div className="bg-white p-6 rounded shadow-lg">
+                        <h2 className="text-xl mb-4">Create Benefit</h2>
+                        <form onSubmit={handleCreate}>
+                            <div>
+                                <label>Benefit Name</label>
+                                <input
+                                    type="text"
+                                    name="benefitName"
+                                    value={formData.benefitName}
+                                    onChange={handleChange}
+                                    required
+                                    className="border p-2 w-full"
+                                />
+                            </div>
+                            <div>
+                                <label>Benefit Description</label>
+                                <input
+                                    type="text"
+                                    name="benefitDescription"
+                                    value={formData.benefitDescription}
+                                    onChange={handleChange}
+                                    required
+                                    className="border p-2 w-full"
+                                />
+                            </div>
+                            <div className="mb-4">
+                                <label htmlFor="benefitType" className="block text-sm font-medium text-gray-700">Benefit Type:</label>
+                                <select
+                                    id="benefitType"
+                                    name="benefitType"
+                                    value={formData.benefitType}
+                                    onChange={handleChange}
+                                    required
+                                    className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 text-sm"
+                                >
+                                    <option value="Others">Others</option>
+                                    <option value="Compensation">Compensation</option>
+                                    <option value="Health">Health</option>
+                                    <option value="Financial">Financial</option>
+                                </select>
+                            </div>
+                            <div>
+                                <label>
+                                    Is Need Request?
+                                    <input
+                                        type="checkbox"
+                                        name="isNeedRequest"
+                                        checked={formData.isNeedRequest}
+                                        onChange={handleChange}
+                                        className="ml-2"
+                                    />
+                                </label>
+                            </div>
+                            <button type="submit" className="mt-4 px-4 py-2 bg-green-500 text-white rounded">Create</button>
+                            <button type="button" onClick={() => setIsOpenModal(false)} className="mt-4 px-4 py-2 bg-red-500 text-white rounded">Cancel</button>
+                        </form>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
